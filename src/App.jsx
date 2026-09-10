fe532b534f1dce3dae593536676f0d746925ed94
kuldip <kuldipgulhane4@gmail.com>
2026-09-10 12:52:19 +0530

    change in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import UseStateHooks from './Component/useStateHooks'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      
+
+<UseStateHooks/>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/UseStateHooks.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+
+const UseStateHooks = () => {
+    const [count, setCount] = useState(0);
+    let [color,setColor] = useState("white");
+
+  return (
+      <>
+          <div id="x" style={{backgroundColor:color}}>
+          <h1>Counter: { count}</h1>
+      
+          <button onClick={() => { setCount(count+1)}}>Increment</button>
+
+          <button onClick={() => {
+              
+              if (count >= 0) {
+               setCount(count - 1)}
+          }}>Decrement</button>
+
+
+          <button onClick={()=>setColor("grey")}   >dark mode</button>
+          <button onClick={()=>setColor("")}   >Light mode</button>
+</div>
+    </>
+  )
+}
+
+export default UseStateHooks
